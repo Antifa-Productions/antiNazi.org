c218f724bfb69c2452fd1f8b84922f54e8526f14
intr0 <1548460-intr0@users.noreply.gitlab.com>
2022-06-06 20:51:40 +0000

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,27 @@
-self.addEventListener('install', function (event) { event.waitUntil(caches.open('v3').then(function (Cache) { return Cache.addAll(['/', 'index.html', '/lit/', '/lit/index.html', 'apple-touch-icon.png', 'touch-icon-ipad-retina.png', 'touch-icon-iphone-retina.png', 'favicon.png', 'icon_x384.png', 'icon_x72.png', 'icon_x96.png', 'icon_x168.png', 'favicon.ico', 'icon_x512.png', 'icon_x192.png', 'https://cdn.iosprivacy.com/img/AntiNaziTwitter.png']) })) });
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v4').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        '/static/index.html',
+        '/lit/',
+        '/lit/index.html',
+        'touch-icon-ipad-retina.png',
+        'touch-icon-iphone-retina.png',
+        'apple-touch-icon.png',
+        'favicon.png',
+        'favicon.ico',
+        'icon_x384.png',
+        'icon_x512.png',
+        'icon_x96.png',
+        'icon_x72.png',
+        'icon_x168.png',
+        'icon_x128.png',
+        'icon_x192.png',
+        'https://cdn.iosprivacy.com/img/AntiNaziTwitter/png',
+        'sw.js',
+        'manifest.webmanifest'
+      ]);
+    })
+  );
