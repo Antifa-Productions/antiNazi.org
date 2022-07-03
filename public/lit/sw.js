288d743d69d435022f5f7c7e616546b5f02168d3
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-07-03 18:05:51 -0400

    Update sw.js

--- public/lit/sw.js
+++ public/lit/sw.js
@@ -1,23 +1,23 @@
-self.addEventListener('install', function (n) {
-  n.waitUntil(caches.open('v4').then(function (n) {
-    return n.addAll(['/',
-    'index.html',
-    '/lit/',
-    '/js/',
-    'touch-icon-ipad-retina.png',
-    'touch-icon-iphone-retina.png',
-    'apple-touch-icon.png',
-    'favicon.png',
-    'favicon.svg',
-    'favicon.ico',
-    'maskable_icon.png',
-    'icon_x72.png',
-    'icon_x96.png',
-    'icon_x128.png',
-    'icon_x192.png',
-    'icon_x384.png',
-    'icon_x512.png',
-    'AntiNaziTwitter.png',
-    'manifest.webmanifest'])
+self.addEventListener("install", function (n) {
+  n.waitUntil(caches.open("v8").then(function (n) {
+    return n.addAll(["/",
+    "/index.html",
+    "/lit/",
+    "/js/",
+    "/touch-icon-ipad-retina.png",
+    "/touch-icon-iphone-retina.png",
+    "/apple-touch-icon.png",
+    "/favicon.png",
+    "/favicon.svg",
+    "/favicon.ico",
+    "/maskable_icon.png",
+    "/icon_x72.png",
+    "/icon_x96.png",
+    "/icon_x128.png",
+    "/icon_x192.png",
+    "/icon_x384.png",
+    "/icon_x512.png",
+    "/AntiNaziTwitter.png",
+    "/manifest.webmanifest"])
   }))
 });
