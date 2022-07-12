87bd6416cb5a4dd034447931c8687e01adb57ded
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-07-12 03:13:31 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,31 @@
-const addResourcesToCache = async (resources) => {
-  const cache = await caches.open("an-v6");
-  await cache.addAll(resources);
+const addResourcesToCache = async(resources) => {
+    const cache = await caches.open("an-v6");
+    await cache.addAll(resources);
 };
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    addResourcesToCache([
-      "/",
-      "/index.html",
-      "/css/style_1.css",
-      "/css/style_2.css",
-      "/js/main.js",
-      "/touch-icon-ipad-retina.png",
-      "/touch-icon-iphone-retina.png",
-      "/apple-touch-icon.png",
-      "/favicon.png",
-      "/favicon.svg",
-      "/favicon.ico",
-      "/maskable_icon.png",
-      "/icon_x72.png",
-      "/icon_x96.png",
-      "/icon_x128.png",
-      "/icon_x192.png",
-      "/icon_x384.png",
-      "/icon_x512.png",
-      "/AntiNaziTwitter.png",
-      "/manifest.webmanifest",
-    ])
-  );
+    event.waitUntil(
+        addResourcesToCache([
+            "/",
+            "/index.html",
+            "/css/style_1.css",
+            "/css/style_2.css",
+            "/js/main.js",
+            "/touch-icon-ipad-retina.png",
+            "/touch-icon-iphone-retina.png",
+            "/apple-touch-icon.png",
+            "/favicon.png",
+            "/favicon.svg",
+            "/favicon.ico",
+            "/maskable_icon.png",
+            "/icon_x72.png",
+            "/icon_x96.png",
+            "/icon_x128.png",
+            "/icon_x192.png",
+            "/icon_x384.png",
+            "/icon_x512.png",
+            "/AntiNaziTwitter.png",
+            "/manifest.webmanifest",
+        ])
+    );
 });
