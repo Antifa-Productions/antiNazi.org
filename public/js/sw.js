a143ac5ad5f6f0802f9823fc2c337866505427f1
intr0 <gitlab@antinazi.org>
2022-07-18 18:46:24 +0000

    Replace sw.js

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,27 +1,27 @@
 const addResourcesToCache = async (resources) => {
-    const cache = await caches.open("an-v10");
+    const cache = await caches.open("an-11");
     await cache.addAll(resources);
 };
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
         addResourcesToCache([
-            "/",
-            "/index.html",
-            "/css/style_2.css",
-            "/js/main.js",
-            "/apple-touch-icon.png",
-            "/favicon.png",
-            "/favicon.svg",
-            "/maskable_icon.png",
-            "/icon_x72.png",
-            "/icon_x96.png",
-            "/icon_x128.png",
-            "/icon_x192.png",
-            "/icon_x384.png",
-            "/icon_x512.png",
-            "/AntiNaziTwitter.png",
-            "/manifest.webmanifest",
+            ".",
+            "index.html",
+            "css/style_2.css",
+            "js/main.js",
+            "apple-touch-icon.png",
+            "favicon.png",
+            "favicon.svg",
+            "maskable_icon.png",
+            "icon_x72.png",
+            "icon_x96.png",
+            "icon_x128.png",
+            "icon_x192.png",
+            "icon_x384.png",
+            "icon_x512.png",
+            "AntiNaziTwitter.png",
+            "manifest.webmanifest",
         ])
     );
 });
