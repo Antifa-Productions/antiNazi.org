3a0495e440095b78ca0decacf488e0251052234e
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-07-18 17:33:31 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,14 +1,8 @@
-self.addEventListener('fetch', function fetcher(event) {
+self.addEventListener("fetch", function fetcher(event) {
     var request = event.request;
-    // check if request 
-    if (request.url.indexOf('antinazi.org') > -1) {
-        // contentful asset detected
-        event.respondWith(
-            caches.match(event.request).then(function (response) {
-                // return from cache, otherwise fetch from network
-                return response || fetch(request);
-            })
-        );
+    if (request.url.indexOf("antinazi.org") > -1) {
+        event.respondWith(caches.match(event.request).then(function(response) {
+            return response || fetch(request);
+        }));
     }
-    // otherwise: ignore event
 });
