0934af090fb7105935fac5f0acbb7a6bf7e65e23
intr0 <1548460-intr0@users.noreply.gitlab.com>
2022-07-25 16:54:44 +0000

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 self.addEventListener("fetch", function fetcher(event) {
-    var request = event.request;
+    var request =
+        event.request;
     if (request.url.indexOf("antinazi.org") > -1) {
         event.respondWith(caches.match(event.request).then(function(response) {
             return response || fetch(request);
