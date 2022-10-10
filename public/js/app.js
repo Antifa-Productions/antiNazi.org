3aed26e0657c6b0c9eb7822997892b3d067a480d
intr0 <1548460-intr0@users.noreply.gitlab.com>
2022-10-10 20:16:40 +0000

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
-self.addEventListener("fetch", function fetcher(event) {
-    var request =
-        event.request;
-    if (request.url.indexOf("antinazi.org") > -1) {
-        event.respondWith(caches.match(event.request).then(function(response) {
-            return response || fetch(request);
-        }));
-    }
-});
+self
+    .addEventListener("fetch", function fetcher(event) {
+        var request = event.request;
+        if (request.url.indexOf("antinazi.org") > -1) {
+            event.respondWith(caches.match(event.request).then(function (response) {
+                return response || fetch(request)
+            }))
+        }
+    });
