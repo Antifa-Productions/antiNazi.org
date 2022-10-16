18e41e2989ea4c570d41369eb0b4e1417f9144a7
intr0 <1548460-intr0@users.noreply.gitlab.com>
2022-10-16 19:25:44 +0000

    Update worker.js

--- public/worker.js
+++ public/worker.js
@@ -1,55 +1,60 @@
-var CACHE_NAME = "an-v1";
-//Just a sample name, the cache name should be more relatable to the application
-var urlsToCache = ["/", "/js", "/css", "/images", "/res"];
+var CACHE_NAME = "an-v2";
+
+var urlsToCache = [
+    ".",
+    "/",
+    "/js",
+    "/css",
+    "/images",
+    "/res",
+    "./well-known"
+];
 
 // Install a service worker
 self.addEventListener("install", (event) => {
     // Perform install steps
-    caches.open(CACHE_NAME).then(function (cache) {
-        Promise.all(
-            urlsToCache.map(function (url) {
+    caches
+        .open(CACHE_NAME)
+        .then(function (cache) {
+            Promise.all(urlsToCache.map(function (url) {
                 cache.add(url);
-            })
-        );
-    });
+            }));
+        });
 });
 
 // Cache lookup and fetch the request
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            // Cache hit - return response
-            if (response) {
+    event.respondWith(caches.match(event.request).then(function (response) {
+        // Cache hit - return response
+        if (response) {
+            return response;
+        }
+        return fetch(event.request).then(function (response) {
+            if (!response || response.status !== 200 || response.type !== "basic") {
                 return response;
             }
-            return fetch(event.request).then(function (response) {
-                if (!response || response.status !== 200 || response.type !== "basic") {
-                    return response;
-                }
 
-                //Clone the response before putting into cache so that response to browser and response to cache happens in two difference streams
-                var responseForCache = response.clone();
-                caches.open(CACHE_NAME).then(function (cache) {
+            // Clone the response before putting into cache so that response to browser and
+            // response to cache happens in two difference streams
+            var responseForCache = response.clone();
+            caches
+                .open(CACHE_NAME)
+                .then(function (cache) {
                     cache.put(event.request, responseForCache);
                 });
-                return response;
-            });
-        })
-    );
+            return response;
+        });
+    }));
 });
 
 // Update a service worker
 self.addEventListener("activate", (event) => {
-    var cacheWhitelist = ["an-v1"];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+    var cacheWhitelist = ["an-v2"];
+    event.waitUntil(caches.keys().then((cacheNames) => {
+        return Promise.all(cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+            }
+        }));
+    }));
 });
