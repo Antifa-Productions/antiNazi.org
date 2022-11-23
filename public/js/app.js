ffcf0b123642b8b853228a10a4d27316c7ed2706
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-11-23 15:16:26 -0500

    Update app.js
    Rewrite script;
    
    Signed-off-by: intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,6 @@
-self
-    .addEventListener("fetch", function (event) {
-        var request = event.request;
-        request
-            .url
-            .indexOf("antinazi.org") > -1 && event.respondWith(caches.match(event.request).then(function (response) {
-                return response || fetch(request)
-            }))
-    });
+self.addEventListener("fetch", function(event) {
+    var request = event.request;
+    request.url.indexOf("https://antinazi.org") > -1 && event.respondWith(caches.match(event.request).then(function(response) {
+        return response || fetch(request);
+    }));
+});
