1fc972d259b5d41d6665fd5e45974014a5cd60b7
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-11-23 15:31:53 -0500

    Delete app.js
    Signed-off-by: intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>

--- public/js/app.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener("fetch", function(event) {
-    var request = event.request;
-    request.url.indexOf("https://antinazi.org") > -1 && event.respondWith(caches.match(event.request).then(function(response) {
-        return response || fetch(request);
-    }));
-});
