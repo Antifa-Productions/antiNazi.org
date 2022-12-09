ff5eea5c47fde0268c1032f9d09717fc67886e83
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2022-12-09 00:27:05 -0500

    Update index.js
    Change code to load sw.js;
    
    Signed-off-by: intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ const registerServiceWorker = async() => {
         try {
             const registration = await navigator
                 .serviceWorker
-                .register("/pep-sw.js", {scope: "/"});
+                .register("/sw.js", {scope: "/"});
             if (registration.installing) {
                 console.log("Service worker installing")
             } else if (registration.waiting) {
