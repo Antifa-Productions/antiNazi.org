d708e93a2e76785c1d5c823e70cb251dee2211ad
intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>
2023-01-20 17:40:30 -0500

    Update service-worker.js
    Signed-off-by: intr0 <65262937+Voltairine-de-Cleyre@users.noreply.github.com>

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,78 +1,32 @@
-importScripts('https://pep.dev/pep-sw-core.js');
-const fileListURL = 'file-list.json';
-self.skipWaiting();
-function fetchAndBust(request) {
-    if (typeof request == 'string') {
-        request = new Request(request)
-    }
-    const url = new URL(request.url);
-    url.search += Math.random();
-    return fetch(url, {
-        headers: request.headers,
-        mode: request.mode,
-        credentials: request.credentials,
-        redirect: request.redirect
-    })
-}
-async function updateCheck() {
-    const r = await fetch(fileListURL);
-    const fileList = await r.json();
-    const staticCacheName = `file-list-v${fileList.version}`;
-    const cacheExists = await caches.has(staticCacheName);
-    if (cacheExists) {
-        return
-    }
-    try {
-        const responses = await Promise.all(fileList.files.map(fetchAndBust));
-        const cache = await caches.open(staticCacheName);
-        return await Promise.all(responses.map((response, i) => {
-            if (!response.ok) {
-                throw Error('Not ok')
-            }
-            return cache.put(fileList.files[i], response)
-        }))
-    } catch (err) {
-        caches.delete(staticCacheName);
-        throw err
-    }
-}
-self.addEventListener('install', event => {
-    event.waitUntil(updateCheck().catch(() => null))
+var CACHE_NAME = 'dependencies-cache';
+self.addEventListener('install', function (event) {
+  console.log('[install] Kicking off service worker registration!');
+  event.waitUntil(caches.open(CACHE_NAME).then(async function (cache) {
+    const response = await fetch('file-list.json');
+    const files = await response.json();
+    console.log('[install] Adding files from JSON file: ', files);
+    return await cache.put(files);
+  }).then(function () {
+    console.log('[install] All required resources have been cached;', 'the Service Worker was successfully installed!'
+    );
+    return self.skipWaiting();
+  })
+  );
 });
-self.addEventListener('fetch', event => {
-    const responsePromise = caches
-        .keys()
-        .then(cacheNames => {
-            const staticCacheNames = cacheNames.filter(n => n.startsWith('static-separate-list-v'));
-            if (!staticCacheNames[0]) {
-                return fetch(event.request)
-            }
-            let staticCacheName = staticCacheNames[0];
-            return Promise
-                .resolve()
-                .then(() => {
-                    if (staticCacheNames.length == 1 || event.request.mode != 'navigate') {
-                        return
-                    }
-                    return clients
-                        .matchAll()
-                        .then(clients => {
-                            if (clients.length > 1) {
-                                return
-                            }
-                            staticCacheName = staticCacheNames[staticCacheNames.length - 1];
-                            return Promise.all(staticCacheNames.slice(0, -1).map(c => caches.delete(c)))
-                        })
-                })
-                .then(() => {
-                    return caches
-                        .open(staticCacheName)
-                        .then(c => c.match(event.request))
-                        .then(response => response || fetch(event.request))
-                })
-        });
-    if (event.request.mode == 'navigate') {
-        event.waitUntil(responsePromise.then(updateCheck))
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(function (response) {
+    if (response) {
+      console.log('[fetch] Returning from Service Worker cache: ', event.request.url
+      );
+      return response;
     }
-    event.respondWith(responsePromise)
+    console.log('[fetch] Returning from server: ', event.request.url);
+    return fetch(event.request);
+  })
+  );
+});
+self.addEventListener('activate', function (event) {
+  console.log('[activate] Activating service worker!');
+  console.log('[activate] Claiming this service worker!');
+  event.waitUntil(self.clients.claim());
 });
